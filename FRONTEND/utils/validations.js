9c3a7d746953c4de9064bdb1e8bbc19c9cf5cae5
Lobna Elabed <70296282+LobnaEla@users.noreply.github.com>
2024-12-11 18:02:44 +0100

    Delete FRONTEND directory

--- FRONTEND/utils/validations.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const saveToken = async (token) => {
-  try {
-    await AsyncStorage.setItem('userToken', token);
-  } catch (error) {
-    console.error('Erreur lors de la sauvegarde du token:', error);
-  }
-};
